98f3cc4f147aea1dc8b708570e75352455b0fc60
5657434 <chiara_magn@libero.it>
2024-02-22 09:41:22 +0100

    added mean function and gitignore

---
+++ stupid_math.c
@@ -0,0 +1,5 @@
+#include "stupid:math.h"
+
+float mean(int a, int b) {
+	retunr (a + b)/2;
+}
---
+++ stupid_math.h
@@ -0,0 +1,7 @@
+#ifndef "_STUPIDMATHH_" //se non è definito il simbolo mathh lo definisco, altrimenti non faccio niente
+#define _STUPIDMATHHH_  //il .h serve per definire che il punto h non va compilato. quando uso una funzione appartenente alla libreria, il .h indica che il simbolo esiste
+// i due <> in una libreria servono al compilatore per capire che si tratta di una libreria standard che non gli fornisco
+
+float mean(int a, int b);
+
+#endif
